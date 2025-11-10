cd9bc395a33aa4aecf81ddd0bb511611de2f531f
liangzenghai <liangzenghai@jd.com>
2025-11-10 20:01:20 +0800

    feat: add post message decrypt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Router = require('@koa/router');
 const { koaBody } = require('koa-body');
 const koaLogger = require('koa-logger');
 const winston = require('winston');
+const xml2js = require('xml2js');
 const crypto = require('@wecom/crypto');
 
 const app = new Koa();
@@ -29,7 +30,6 @@ app.use(koaBody());
 app.use(koaLogger());
 
 router.get('/scrm/callback', (ctx, next) => {
-    console.log('get', ctx.query);
     logger.info('Get params', ctx.query);
     // 从 query 中获取相关参数
     const { msg_signature, timestamp, nonce, echostr } = ctx.query;
@@ -47,7 +47,25 @@ router.get('/scrm/callback', (ctx, next) => {
 });
 
 router.post('/scrm/callback', (ctx, next) => {
-    console.log('post', ctx.body);
+    const { msg_signature, timestamp, nonce, echostr } = ctx.query;
+    const post_body = ctx.request.body;
+    const signature = crypto.getSignature(token, timestamp, nonce, post_body);
+    if (msg_signature == signature) {
+        logger.info("Good signature.", signature);
+        xml2js.parseString(post_body, (err, result) => {
+            if (err) {
+                logger.error('ERROR parsing xml: ', post_body);
+                ctx.body = 'error';
+                return;
+            }
+            const { message } = crypto.decrypt(aeskey, result.Encrypt);
+            logger.info("Clear message: ", message);
+            ctx.body = 'success';
+        })
+        
+    } else {
+        ctx.body = 'error';
+    }
 });
 
 app
