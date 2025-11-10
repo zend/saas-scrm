68fe6804c41cb34d76ce4250633d8cc8f77cdca1
zend <combbs@gmail.com>
2025-11-10 19:22:58 +0800

    first init

---
+++ index.js
@@ -0,0 +1,57 @@
+const Koa = require('koa');
+const Router = require('@koa/router');
+const { koaBody } = require('koa-body');
+const koaLogger = require('koa-logger');
+const winston = require('winston');
+const crypto = require('@wecom/crypto');
+
+const app = new Koa();
+const router = new Router();
+const token = 'sTx1DXFszX1CVi9S01bv6MvOh';
+const aeskey = 'gFpakt4dFMIEQFzyxFimLLs0w5c3g441BW7Bp9gGS2A';
+
+const myFormat = winston.format.printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+const logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.label({ label: 'right meow!' }),
+        winston.format.timestamp(),
+        myFormat
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'app.log' })
+    ]
+});
+
+app.use(koaBody());
+app.use(koaLogger());
+
+router.get('/scrm/callback', (ctx, next) => {
+    console.log('get', ctx.query);
+    logger.info('Get params', ctx.query);
+    // 从 query 中获取相关参数
+    const { msg_signature, timestamp, nonce, echostr } = ctx.query;
+    const signature = crypto.getSignature(token, timestamp, nonce, echostr);
+
+    if (signature === msg_signature) {
+        console.info('签名验证成功')
+        // 在应用详情页找到对应的EncodingAESKey
+        // 如果签名校验正确，解密 message
+        const { message } = crypto.decrypt(aeskey, echostr);
+        console.log('message', message);
+        // 返回 message 信息
+        ctx.body = message;
+    }
+});
+
+router.post('/scrm/callback', (ctx, next) => {
+    console.log('post', ctx.body);
+});
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+app.listen(24601);
